462ff3e0db1918da7582cfc97af99897a7741d54
Leon <leon.nguyen.gos@gmail.com>
2024-03-31 23:02:13 +0700

    login - auto go to home if user already signed in

--- lib/src/router/router.dart
+++ lib/src/router/router.dart
@@ -1,4 +1,5 @@
 import 'package:go_router/go_router.dart';
+import 'package:survly/src/domain_manager.dart';
 import 'package:survly/src/features/authentication/view/login_screen.dart';
 import 'package:survly/src/features/authentication/view/signup_view.dart';
 import 'package:survly/src/features/home/view/home_view.dart';
@@ -6,7 +7,15 @@ import 'package:survly/src/router/router_name.dart';
 
 class AppRouter {
   final router = GoRouter(
-    initialLocation: AppRouteNames.login.path,
+    // initialLocation: AppRouteNames.splash.path,
+    redirect: (context, state) async {
+      DomainManager domain = DomainManager();
+      var loginInfo = await domain.authenticationLocal.readLoginInfo();
+      if (loginInfo?.email != "") {
+        return AppRouteNames.home.path;
+      }
+      return AppRouteNames.login.path;
+    },
     routes: <RouteBase>[
       GoRoute(
         name: AppRouteNames.home.name,
