5b98d29f4948ac29c79287ab2b452e39f4d069f7
Leon <leon.nguyen.gos@gmail.com>
2024-04-01 12:06:04 +0700

    home - bottom nav bar UI

--- lib/src/features/home/view/home_view.dart
+++ lib/src/features/home/view/home_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:survly/src/features/home/logic/home_bloc.dart';
 import 'package:survly/src/features/home/logic/home_state.dart';
+import 'package:survly/widgets/app_app_bar.dart';
 import 'package:survly/widgets/app_loading_circle.dart';
 
 class HomeView extends StatefulWidget {
@@ -12,6 +13,9 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+
+  var currentIndex = 0;
+
   @override
   void initState() {
     super.initState();
@@ -21,12 +25,32 @@ class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: const AppAppBarWidget(
+        leading: SizedBox(),
+        title: "Survey",
+      ),
       body: BlocBuilder<HomeBloc, HomeState>(
         builder: (context, state) {
           return state.status == HomeStatus.loading
               ? const AppLoadingCircle()
               : const Center(child: Text("Hello world"));
         },
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (value) {
+          
+        },
+        currentIndex: currentIndex,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.note_alt_outlined),
+            label: "Survey",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.image),
+            label: "Outlet",
+          ),
+        ],
       ),
     );
   }
