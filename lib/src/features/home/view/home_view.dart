bf29853d96a44bce6c827835f48bfc01be092b13
Leon <leon.nguyen.gos@gmail.com>
2024-04-02 10:37:38 +0700

    home - show admin info

--- lib/src/features/home/view/home_view.dart
+++ lib/src/features/home/view/home_view.dart
@@ -50,9 +50,7 @@ class _HomeViewState extends State<HomeView> {
         leading: SizedBox(),
         title: "Survey",
       ),
-      body: const Center(
-        child: Text("Hello world"),
-      ),
+      body: _buildHomeBody(),
       bottomNavigationBar: BottomNavigationBar(
         onTap: (value) {
           setState(() {
@@ -71,6 +69,45 @@ class _HomeViewState extends State<HomeView> {
           ),
         ],
       ),
+    );
+  }
+
+  Widget _buildHomeBody() {
+    return Column(
+      children: [
+        _buildAdminInfo(),
+      ],
+    );
+  }
+
+  Widget _buildAdminInfo() {
+    return Row(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: const Color(0xff7c94b6),
+            image: const DecorationImage(
+              image: NetworkImage(
+                  "https://images.pexels.com/photos/18254876/pexels-photo-18254876/free-photo-of-waves-by-the-rocky-beach.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"),
+              fit: BoxFit.cover,
+            ),
+            borderRadius: const BorderRadius.all(Radius.circular(50.0)),
+            border: Border.all(
+              color: Colors.red,
+              width: 4.0,
+            ),
+          ),
+        ),
+        ClipRRect(
+          borderRadius: const BorderRadius.all(Radius.circular(50)),
+          child: Image.network(
+            "https://images.pexels.com/photos/18254876/pexels-photo-18254876/free-photo-of-waves-by-the-rocky-beach.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            width: 100,
+            height: 100,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ],
     );
   }
 }
