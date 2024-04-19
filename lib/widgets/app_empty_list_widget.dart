f4ab087aa9ef432eaa384720f537b14bd7e8418f
Leon <leon.nguyen.gos@gmail.com>
2024-04-19 17:53:15 +0700

    submit survey - resolve comment

--- lib/widgets/app_empty_list_widget.dart
+++ lib/widgets/app_empty_list_widget.dart
@@ -1,22 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:survly/src/localization/localization_utils.dart';
 
 class AppEmptyListWidget extends StatelessWidget {
   const AppEmptyListWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
+    return Center(
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Icon(
+          const Icon(
             Icons.sentiment_very_satisfied_outlined,
             size: 64,
             color: Colors.grey,
           ),
           Text(
-            "Nothing here",
-            style: TextStyle(color: Colors.grey),
+            S.of(context).labelEmptyList,
+            style: const TextStyle(color: Colors.grey),
           ),
         ],
       ),
