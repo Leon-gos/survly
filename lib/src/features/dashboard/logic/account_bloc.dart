89b952c581875af92ed8759f5d20fc76069877ef
Leon <leon.nguyen.gos@gmail.com>
2024-05-02 21:59:25 +0700

    hotfix - delete fcm token after logout

--- lib/src/features/dashboard/logic/account_bloc.dart
+++ lib/src/features/dashboard/logic/account_bloc.dart
@@ -122,6 +122,7 @@ class AccountBloc extends Cubit<AccountState> {
     try {
       await domainManager.authentication.logout();
       await domainManager.authenticationLocal.clearLoginInfo();
+      await NotificationService.deleteToken();
       UserBaseSingleton.instance().userBase = null;
       AppCoordinator.goNamed(AppRouteNames.login.path);
     } catch (e) {
