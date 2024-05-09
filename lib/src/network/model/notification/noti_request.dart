8018dcb7cad55a8d545394e56bc5cfb8331f5b81
Leon <leon.nguyen.gos@gmail.com>
2024-05-09 15:33:38 +0700

    noti list - create noti in firestore when admin respone user request

--- lib/src/network/model/notification/noti_request.dart
+++ lib/src/network/model/notification/noti_request.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:survly/src/network/model/notification/notification.dart';
 
 class NotiRequest extends Notification {
